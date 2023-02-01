432f0dd19fa6853b97a4d40894298b539edc01c4
Simba Wangu <simbawangulu@gmail.com>
2023-02-01 13:38:13 +0300

    add mongodb connection to app

--- index.js
+++ index.js
@@ -5,17 +5,31 @@ const images_1 = require("./src/routes/images");
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 // *Constants
 const app = express();
 const port = 3000;
 // *Middleware
+const mongoUrl = process.env.MONGODB_URL;
 app.use(cors());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+try {
+    mongoose.connect(mongoUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    console.log('MongoDB connected');
+}
+catch (err) {
+    console.log(err);
+}
 // *Routes
 app.use('/image', images_1.imagesRouter);
 app.listen(port, () => {
     console.log(`Express server listening at http://localhost:${port}`);
 });
+// !simberella
+// !YSSkVNL41hFYSNHP
