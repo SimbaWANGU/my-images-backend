c03b2b1ff9e48a47c714c3746288a7a7827172ee
Simba Wangu <simbawangulu@gmail.com>
2023-02-01 14:26:04 +0300

    add logic for saving images to db if user exists and respond with images if user doesnt exist

--- src/controllers/images.js
+++ src/controllers/images.js
@@ -2,9 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createImage = void 0;
 const ImageApiCall_1 = require("../api/ImageApiCall");
+const Images_1 = require("../model/Images");
 const createImage = async (req, res) => {
     const prompt = req.body.prompt;
     const numImages = req.body.num_images;
+    const email = req.body.email;
     if (prompt.length === 0 || numImages < 1 || numImages > 10) {
         res.status(400).json({
             error: 'Bad Request'
@@ -12,8 +14,32 @@ const createImage = async (req, res) => {
     }
     else {
         (0, ImageApiCall_1.createImageFromOpenAi)(prompt, Number(numImages))
-            .then((data) => {
-            console.log('please', data);
+            .then(async (data) => {
+            if (typeof email === 'string') {
+                console.log(email, data);
+                await Images_1.ImageModel.create({
+                    email,
+                    prompt,
+                    images: data
+                })
+                    .then(() => {
+                    res.status(201).json({
+                        success: 'Images Generated'
+                    });
+                })
+                    .catch(() => {
+                    res.status(500).json({
+                        error: 'An error occurred'
+                    });
+                });
+            }
+            else {
+                console.log(data);
+                res.status(201).json({
+                    success: 'Images Generated',
+                    images: data
+                });
+            }
         })
             .catch((err) => {
             console.log(err);
