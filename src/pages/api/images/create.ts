a8cc3559a23e665a42447ec83462ea167a9445d4
Simba Wangu <simbawangulu@gmail.com>
2023-02-23 11:12:11 +0300

    refactor file structure

--- src/pages/api/images/create.ts
+++ src/pages/api/images/create.ts
@@ -1,5 +1,7 @@
-import { imagesControllerPost } from '@/pages/controllers/imagesController'
+import { imagesControllerPost } from "@/controllers/imagesController"
 import type { NextApiRequest, NextApiResponse } from 'next'
+
+interface Data {}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   if (req.method === 'POST') {
