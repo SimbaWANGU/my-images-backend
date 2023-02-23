7d628d98bd1d5887b6d527c8e9b382dfb437e6ff
Simba Wangu <simbawangulu@gmail.com>
2023-02-23 10:50:57 +0300

    Update create.ts

--- src/pages/api/images/create.ts
+++ src/pages/api/images/create.ts
@@ -1,6 +1,8 @@
 import { imagesControllerPost } from '@/pages/controllers/imagesController'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+interface Data {}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   if (req.method === 'POST') {
     await imagesControllerPost(req, res)
